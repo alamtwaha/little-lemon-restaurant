85f1267400d9dbd0998f5b976a5340b8297232fc
Md Mahbub Alam <88573006+alamtwaha@users.noreply.github.com>
2023-06-08 00:25:06 +0600

    Created 4 Components for 4 section of the little lemon restaurant which are Nav, Header, Main and footer. All the section and updated with the semactic HLTM structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-     Home Page
-    </div>
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+      <div>
+          <h1> Navigation</h1>
+      </div>
+    )
+  }
+  
+  export default Footer
