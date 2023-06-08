e9b821f799a2b6e44d0121d4aa034bc99e425cd8
Md Mahbub Alam <88573006+alamtwaha@users.noreply.github.com>
2023-06-08 18:04:57 +0600

    Addting navigation links and css styles.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,20 @@
-
+import logo from '../logo.svg';
+import './Nav.css';
 
 const Nav = () => {
     return (
-      <div>
-          <h1> Navigation</h1>
-      </div>
+      <>
+       <div className="nav-container">
+        <img src={logo} alt="Little Lemon Restaurant Logo"/>
+        <nav>
+            <ul>
+              <li><a href="/home">Home</a></li>
+              <li><a href="/about">About Us</a></li>
+              <li><a href="/blogs">Blogs</a></li>
+            </ul>
+        </nav>
+       </div>
+      </>
     )
   }
   
