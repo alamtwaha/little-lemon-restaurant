2aec2f842dbc4bb04f6b3bc1e01b5220aecbeeb4
Md Mahbub Alam <88573006+alamtwaha@users.noreply.github.com>
2023-07-03 22:57:29 +0600

    Prettifying nav bar andheader style using CSS.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,18 +5,20 @@ const Nav = () => {
     return (
       <>
        <div className="nav-container">
-        <img src={logo} alt="Little Lemon Restaurant Logo"/>
-        <nav>
-            <ul>
-              <li><a href="/home">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/menu">Menu</a></li>
-              <li><a href="/reservation">Reservation</a></li>
-              <li><a href="/orderOnline">Order Online</a></li>
-              <li><a href="/login">Login</a></li>
-            </ul>
-        </nav>
-       </div>
+          <div className="nav-content">
+            <img src={logo} alt="Little Lemon Restaurant Logo"/>
+            <nav>
+                <ul>
+                  <li><a href="/home">Home</a></li>
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/menu">Menu</a></li>
+                  <li><a href="/reservation">Reservation</a></li>
+                  <li><a href="/orderOnline">Order Online</a></li>
+                  <li><a href="/login">Login</a></li>
+                </ul>
+            </nav>
+          </div>
+        </div>
       </>
     )
   }
