ebc87fa266a9fdb059035174f652ec6fb47eb86b
Md Mahbub Alam <88573006+alamtwaha@users.noreply.github.com>
2023-07-03 21:50:49 +0600

    updating Heder and navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
-
+import header from  '../header.jpg';
+import "./Header.css"
 
 const Header = () => {
     return (
-      <div>
-          <h1> Header</h1>
-      </div>
+      <>
+        <div className="header">
+          <div className='header-text'>
+              <h1> Savor the tangy taste of life at Little Lemon Restaurant</h1>
+              <p>Indulge in a delectable meal with our diverse menu options, including the juicy, Grilled Salmon, Beef Wellington, the spicy and creamy Chicken</p>
+              <button>Reserve a Table</button>
+          </div>
+          
+          <div className='header-img'>
+             <img src={header} alt="Header" />
+          </div>
+        </div>
+      </>
     )
   }
   
