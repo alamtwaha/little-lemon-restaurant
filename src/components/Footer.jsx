812cc3d8197145a5f8a667565a9301925250612b
Md Mahbub Alam <88573006+alamtwaha@users.noreply.github.com>
2023-07-11 17:27:12 +0600

    Finished the Footer of the Little Lemon Restaurant Website.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,40 @@
-
+import Logo from "../logo.svg";
+import "./Footer.css";
 
 const Footer = () => {
     return (
-      <div>
-          <h1> Navigation</h1>
-      </div>
+      <>
+        <div className="Footer">
+          <div className="Footer-Container">
+            <div className="Footer-Logo">
+              <img src={Logo} alt="" className="logo" />
+              <p>Little Lemon restaurant in fine dine-in Restaurant
+                in NYC Also take online orders. Indulge in a delectable 
+                meal with our diverse menu options, including the juicy, 
+                Grilled Salmon, Beef Wellington, the spicy and creamy Chicken</p>
+            </div>
+
+            <div className="Footer-Link">
+              <ul>
+                <li>Mission</li>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions</li>
+                <li>Refund Policy</li>
+              </ul>
+            </div>
+
+            <div className="Footer-Contact">
+              <h2>Contact</h2>
+              <p>info@littlelemonrestaurant.com</p>
+              <p>(301) 862-4338</p>
+              <p>23415 Three Notch Rd, California</p>
+
+            </div>
+
+            {/* <h1> Navigation</h1> */}
+          </div>
+        </div>
+      </>
     )
   }
   
